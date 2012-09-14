3e3e5cc44d21ec5fcb7859c5acd8ffe9dda14e18
Jochen Schalanda <j.schalanda@smarchive.de>
2012-09-14 15:49:39 +0200

    Fix puppet-lint errors and warnings

--- manifests/download.pp
+++ manifests/download.pp
@@ -1,137 +1,139 @@
-/*
+# == Definition: archive::download
+#
+# Archive downloader with integrity verification.
+#
+# Parameters:
+#
+# - *$url:
+# - *$digest_url:
+# - *$digest_content: Default value ''
+# - *$digest_string: Default value ''
+# - *$digest_type: Default value 'md5'.
+# - *$timeout: Default value 120.
+# - *$src_target: Default value '/usr/src'.
+# - *$allow_insecure: Default value false.
+#
+# Example usage:
 
-== Definition: archive::download
-
-Archive downloader with integrity verification.
-
-Parameters:
-
-- *$url: 
-- *$digest_url:
-- *$digest_string: Default value "" 
-- *$digest_type: Default value "md5".
-- *$timeout: Default value 120.
-- *$src_target: Default value "/usr/src".
-- *$allow_insecure: Default value false.
-
-Example usage:
-
-  archive::download {"apache-tomcat-6.0.26.tar.gz":
-    ensure => present,
-    url => "http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.tar.gz",
-  }
-  
-  archive::download {"apache-tomcat-6.0.26.tar.gz":
-    ensure => present,
-    digest_string => "f9eafa9bfd620324d1270ae8f09a8c89",
-    url => "http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.tar.gz",
-  }
-   
-*/
+#   archive::download {'apache-tomcat-6.0.26.tar.gz':
+#     ensure => present,
+#     url => 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.tar.gz',
+#   }
+#
+#   archive::download {'apache-tomcat-6.0.26.tar.gz':
+#     ensure => present,
+#     digest_string => 'f9eafa9bfd620324d1270ae8f09a8c89',
+#     url => 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.tar.gz',
+#   }
 define archive::download (
-  $ensure=present, 
-  $url, 
-  $checksum=true,
-  $digest_url="",
-  $digest_string="",
-  $digest_type="md5",
-  $timeout=120,
-  $src_target="/usr/src",
-  $allow_insecure=false) {
+  $url,
+  $ensure         = present,
+  $checksum       = true,
+  $digest_url     = '',
+  $digest_content = '',
+  $digest_string  = '',
+  $digest_type    = 'md5',
+  $timeout        = 120,
+  $src_target     = '/usr/src',
+  $allow_insecure = false) {
 
   $insecure_arg = $allow_insecure ? {
-    true => "-k",
-    default => "",
+    true    => '-k',
+    default => '',
   }
 
   if !defined(Package['curl']) {
-    package{'curl':
-      ensure => present,
-    }
+    package{'curl': ensure => present }
   }
 
   case $checksum {
     true : {
       case $digest_type {
-        'md5','sha1','sha224','sha256','sha384','sha512' : { 
-          $checksum_cmd = "${digest_type}sum -c ${name}.${digest_type}" 
+        'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512' : {
+          $checksum_cmd = "${digest_type}sum -c ${name}.${digest_type}"
         }
-        default: { fail "Unimplemented digest type" }
+        default: { fail('Unimplemented digest type') }
       }
-    
-      if $digest_url != "" and $digest_content != "" {
-        fail "digest_url and digest_content should not be used together !"
+
+      if $digest_url != '' and $digest_content != '' {
+        fail('digest_url and digest_content should not be used together!')
       }
-    
-      if $digest_content == "" {
-    
+
+      if $digest_content == '' {
+
         case $ensure {
           present: {
-    
-            if $digest_url == "" {
+
+            if $digest_url == '' {
               $digest_src = "${url}.${digest_type}"
             } else {
               $digest_src = $digest_url
             }
-    
-            exec {"download digest of archive $name":
+
+            exec {"download digest of archive ${name}":
               command => "curl ${insecure_arg} -o ${src_target}/${name}.${digest_type} ${digest_src}",
               creates => "${src_target}/${name}.${digest_type}",
               timeout => $timeout,
-              notify  => Exec["download archive $name and check sum"],
-              require => Package["curl"],
+              notify  => Exec["download archive ${name} and check sum"],
+              require => Package['curl'],
             }
-    
+
           }
           absent: {
             file{"${src_target}/${name}.${digest_type}":
               ensure => absent,
-              purge => true,
-              force => true,
+              purge  => true,
+              force  => true,
             }
           }
+          default: { fail("Unknown ensure value: '${ensure}'") }
         }
       }
-    
-      if $digest_string != "" {
+
+      if $digest_string != '' {
         case $ensure {
           present: {
             file {"${src_target}/${name}.${digest_type}":
-              ensure => $ensure,
+              ensure  => $ensure,
               content => "${digest_string} *${name}",
-              notify => Exec["download archive $name and check sum"],
+              notify  => Exec["download archive ${name} and check sum"],
             }
           }
           absent: {
             file {"${src_target}/${name}.${digest_type}":
               ensure => absent,
-              purge => true,
-              force => true,
+              purge  => true,
+              force  => true,
             }
           }
+          default: { fail("Unknown ensure value: '${ensure}'") }
         }
       }
     }
-    false :  { notice "No checksum for this archive" }
-    default: { fail ( "Unknown checksum value: '${checksum}'" ) }
+    false :  { notice('No checksum for this archive') }
+    default: { fail("Unknown checksum value: '${checksum}'") }
   }
- 
+
   case $ensure {
     present: {
-      exec {"download archive $name and check sum":
-        command   => "curl ${insecure_arg} -o ${src_target}/${name} ${url}",
-        creates   => "${src_target}/${name}",
-        logoutput => true,
-        timeout   => $timeout,
-        require   => Package["curl"],
-        notify => $checksum ? {
-          true    => Exec["rm-on-error-${name}"],
-          default => undef,
-        },
-        refreshonly => $checksum ? {
-          true    => true,
-          default => undef,
-        },
+      $notify = $checksum ? {
+        true    => Exec["rm-on-error-${name}"],
+        default => undef,
+      }
+
+      $refreshonly = $checksum ? {
+        true    => true,
+        default => undef,
+      }
+
+      exec {"download archive ${name} and check sum":
+        command     => "curl ${insecure_arg} -o ${src_target}/${name} ${url}",
+        creates     => "${src_target}/${name}",
+        logoutput   => true,
+        timeout     => $timeout,
+        require     => Package['curl'],
+        notify      => $notify,
+        refreshonly => $refreshonly,
       }
 
       exec {"rm-on-error-${name}":
@@ -144,10 +146,10 @@ define archive::download (
     absent: {
       file {"${src_target}/${name}":
         ensure => absent,
-        purge => true,
-        force => true,
+        purge  => true,
+        force  => true,
       }
     }
-    default: { fail ( "Unknown ensure value: '${ensure}'" ) }
+    default: { fail("Unknown ensure value: '${ensure}'") }
   }
 }
