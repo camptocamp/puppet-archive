9471ea592c894eb9efa7d567452ec8006d2308f7
Elias Probst <mail@eliasprobst.eu>
2013-09-28 00:32:39 +0200

    Dropped the 'ensure' param from 'archive::package' and added manifest documentation based on the styleguide template.

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,5 +1,31 @@
+# == Definition: archive::package
+#
+# This definition provides the dependencies for extracting the given archive.
+#
+# === Parameters
+#
+# [*extension*]
+#   The extension of the archive to be extracted - used to determine the package
+#   dependencies (e.g. 'tar.gz': 'tar', 'rpm': 'rpm2cpio, cpio', ...
+#
+# === Examples
+#
+# Install the package requirements for extracting an 'rpm' file
+#
+# archive::package { 'some-file':
+#   extension => 'rpm',
+# }
+#
+# === Authors
+#
+# Elias Probst <mail@eliasprobst.eu>
+#
+# === Copyright
+#
+# Copyright 2013, Elias Probst
+#
+
 define archive::package (
-    $ensure    = present,
     $extension = 'tar.gz'
 ) {
     case $ensure {
