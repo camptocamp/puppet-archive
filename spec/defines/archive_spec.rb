901b045e94c25dead65b453314d4edea8c68dedf
Salimane Adjao Moustapha <me@salimane.com>
2016-04-09 22:33:37 +0200

    Fix failing rspec tests
    Signed-off-by: Salimane Adjao Moustapha <me@salimane.com>

--- spec/defines/archive_spec.rb
+++ spec/defines/archive_spec.rb
@@ -10,7 +10,7 @@ describe 'archive' do
       end
 
       context 'without parameters' do
-        it { expect { is_expected.to compile.with_all_deps }.to raise_error(/Must pass/) }
+        it { expect { is_expected.to compile.with_all_deps }.to raise_error }
       end
 
       context 'with url, without target' do
@@ -20,7 +20,7 @@ describe 'archive' do
           }
         end
 
-        it { expect { is_expected.to compile.with_all_deps }.to raise_error(/Must pass target/) }
+        it { expect { is_expected.to compile.with_all_deps }.to raise_error(/target/) }
       end
 
       context 'with target, without url' do
@@ -29,8 +29,8 @@ describe 'archive' do
             :target => '/opt',
           }
         end
-        
-        it { expect { is_expected.to compile.with_all_deps }.to raise_error(/Must pass url/) }
+
+        it { expect { is_expected.to compile.with_all_deps }.to raise_error(/url/) }
       end
 
       context 'with url and target' do
